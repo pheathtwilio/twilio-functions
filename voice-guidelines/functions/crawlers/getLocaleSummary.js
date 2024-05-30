c8a9f9be16b18c4846fdf5dcfa9d80a7f7c930a9
pheathtwilio <pheath@twilio.com>
2024-05-30 10:47:54 -0500

    added locale summary

---
+++ voice-guidelines/functions/crawlers/getLocaleSummary.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+exports.handler = async function (context, event, callback) {
+
+    const localeSummary = []
+    const url = event.URL
+
+    try{
+
+    const { data } = await axios.get(url)
+    const $ = cheerio.load(data)
+
+    const localeSummaryTable = '#guideline-tables > div > div:nth-child(1) > div > div > table > tbody'
+
+    $(localeSummaryTable).find('tr').each((index, row) => {
+
+        localeSummary.push({
+            [($(row).find('td:nth-child(1)').text()).trim()]:($(row).find('td:nth-child(2)').text()).trim()
+        })
+
+    })
+
+    }catch (error) {
+        console.error(`Error: ${error}`)
+        callback(error, '')
+    }   
+
+  callback(null, JSON.stringify({"Locale Summary": localeSummary}));
+};
