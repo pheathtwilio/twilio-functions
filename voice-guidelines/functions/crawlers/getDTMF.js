214bc368f909bee58b2b66a0b8dd21c47057fce7
pheathtwilio <pheath@twilio.com>
2024-05-30 14:26:54 -0500

    Added get DTMF data

---
+++ voice-guidelines/functions/crawlers/getDTMF.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+exports.handler = async function (context, event, callback) {
+
+    const dtmfs = []
+    const url = event.URL
+
+    try{
+
+        const { data } = await axios.get(url)
+        const $ = cheerio.load(data)
+
+        const dtmfTable = '#guideline-tables > div > div:nth-child(4) > div > div > table > tbody'
+
+        $(dtmfTable).find('tr').each((index, row) => {
+
+            dtmfs.push({
+                [($(row).find('td:nth-child(1)').text()).trim()]:($(row).find('td:nth-child(2)').text()).trim()
+            })
+
+        })
+
+    }catch (error) {
+        console.error(`Error: ${error}`)
+        callback(error)
+    }
+
+  callback(null, JSON.stringify({"Caller ID": dtmfs}));
+}
