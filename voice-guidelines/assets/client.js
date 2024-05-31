9567a3501d773549fc99e778f364810dff6d2ac4
pheathtwilio <pheath@twilio.com>
2024-05-31 12:02:06 -0500

    added string validation and table decoration for Locale Summary

--- voice-guidelines/assets/client.js
+++ voice-guidelines/assets/client.js
@@ -52,42 +52,55 @@ const getLocaleSummary = async (url) => {
     return await response.json()
 }
 
+const decorateLocaleSummary = (parentNode, localeSummary) => {
+
+    const table = document.createElement('table')
+    table.className = 'table table-striped table-dark table-hover'
+    const tbody = document.createElement('tbody')
+
+    let tr = document.createElement('tr')
+    let th = document.createElement('th')
+    th.innerHTML = 'Locale Summary'
+    tr.appendChild(th)
+    th = document.createElement('th')
+    tr.appendChild(th)
+    tbody.appendChild(tr)
+
+    localeSummary.Summary.forEach((element) => {
+        let tr = document.createElement('tr')
+
+        let td = document.createElement('td')
+        let key = Object.keys(element)[0]
+        td.innerHTML =  validateString(JSON.stringify(key)) 
+
+        tr.appendChild(td)
+
+        td = document.createElement('td')
+        td.innerHTML = validateString(JSON.stringify(element[key]))
+        tr.appendChild(td)
+
+        tbody.appendChild(tr)
+    })
+    
+    table.appendChild(tbody)
+    parentNode.appendChild(table)
+
+}
+
 const listElementClicked = async (event) => {
 
     const listElement = event.target.parentNode
-    const button = event.target
 
     const url = listElement.id
+
+    console.log(listElement)
 
     if(url == 'countries-list'){
         console.log('Error, no country specified')
     }else{
 
-        const locale = await getLocaleSummary(listElement.id)
-
-        const table = document.createElement('table')
-        table.className = 'table table-dark'
-        const tbody = document.createElement('tbody')
-
-        locale.Summary.forEach((element) => {
-            let tr = document.createElement('tr')
-
-            let td = document.createElement('td')
-            let key = Object.keys(element)[0]
-            td.innerHTML = JSON.stringify(key)
-            tr.appendChild(td)
-
-            td = document.createElement('td')
-            td.innerHTML = JSON.stringify(element[key])
-            tr.appendChild(td)
-
-            tbody.appendChild(tr)
-        })
-        
-        table.appendChild(tbody)
-        listElement.appendChild(table)
-        
-        // get the country specific details
+        const localeSummary = await getLocaleSummary(listElement.id)
+        decorateLocaleSummary(listElement, localeSummary)
 
     }
 
@@ -134,6 +147,10 @@ const stopLoading = () => {
         loadingElement.removeChild(loadingElement.firstChild);
     }
 
+}
+
+const validateString = (strToReplace) => {
+    return strToReplace.replace(/['"]+/g, '')
 }
 
 window.addEventListener("load", async () => {
