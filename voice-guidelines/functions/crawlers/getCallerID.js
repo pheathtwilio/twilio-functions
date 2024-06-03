c81a75af63c157c73bde7e80180804f5b0ea37fe
pheathtwilio <pheath@twilio.com>
2024-06-03 15:45:03 -0500

    fixed get Caller ID data for USA

--- voice-guidelines/functions/crawlers/getCallerID.js
+++ voice-guidelines/functions/crawlers/getCallerID.js
@@ -12,14 +12,25 @@ exports.handler = async function (context, event, callback) {
         const $ = cheerio.load(data)
 
         const callerTable = '#guideline-tables > div > div:nth-child(3) > div > div > table > tbody'
+        const callerTable_2 = '#base-page-7c9c6637a6 > main > section.section-wrapper.default > div > div:nth-child(3) > div > div > table > tbody'
 
-        $(callerTable).find('tr').each((index, row) => {
+        if($(callerTable).find('tr').children().length > 0){
+            $(callerTable).find('tr').each((index, row) => {
 
-            callerids.push({
-                [($(row).find('td:nth-child(1)').text()).trim()]:{inbound:$(row).find('td:nth-child(2)').text().trim(), outbound:$(row).find('td:nth-child(3)').text().trim()}
+                callerids.push({
+                    [($(row).find('td:nth-child(1)').text()).trim()]:{inbound:$(row).find('td:nth-child(2)').text().trim(), outbound:$(row).find('td:nth-child(3)').text().trim()}
+                })
+
             })
+        }else{
+            $(callerTable_2).find('tr').each((index, row) => {
 
-        })
+                callerids.push({
+                    [($(row).find('td:nth-child(1)').text()).trim()]:{inbound:$(row).find('td:nth-child(2)').text().trim(), outbound:$(row).find('td:nth-child(3)').text().trim()}
+                })
+
+            })
+        }
 
     }catch (error) {
         console.error(`Error: ${error}`)
