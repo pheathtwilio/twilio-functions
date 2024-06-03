c793be84edeecff7e2485219f9eb7f5489772a68
pheathtwilio <pheath@twilio.com>
2024-06-03 15:39:04 -0500

    fixed localeSummary for USA page

--- voice-guidelines/functions/crawlers/getLocaleSummary.js
+++ voice-guidelines/functions/crawlers/getLocaleSummary.js
@@ -12,14 +12,26 @@ exports.handler = async function (context, event, callback) {
     const $ = cheerio.load(data)
 
     const localeSummaryTable = '#guideline-tables > div > div:nth-child(1) > div > div > table > tbody'
+    const localeSummaryTable_2 = '#base-page-7c9c6637a6 > main > section.section-wrapper.default > div > div:nth-child(1) > div > div > table > tbody'
 
-    $(localeSummaryTable).find('tr').each((index, row) => {
+    if($(localeSummaryTable).find('tr').children().length > 0){
+        $(localeSummaryTable).find('tr').each((index, row) => {
 
-        localeSummary.push({
-            [($(row).find('td:nth-child(1)').text()).trim()]:($(row).find('td:nth-child(2)').text()).trim()
+            localeSummary.push({
+                [($(row).find('td:nth-child(1)').text()).trim()]:($(row).find('td:nth-child(2)').text()).trim()
+            })
+    
         })
+    }else{
+        $(localeSummaryTable_2).find('tr').each((index, row) => {
 
-    })
+            localeSummary.push({
+                [($(row).find('td:nth-child(1)').text()).trim()]:($(row).find('td:nth-child(2)').text()).trim()
+            })
+    
+        })
+    }
+    
 
     }catch (error) {
         console.error(`Error: ${error}`)
