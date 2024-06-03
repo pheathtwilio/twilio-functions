b14ef1df0a25f279053854f61634bf1773f4f7c3
pheathtwilio <pheath@twilio.com>
2024-06-03 15:42:34 -0500

    fixed getReachability for USA page

--- voice-guidelines/functions/crawlers/getReachability.js
+++ voice-guidelines/functions/crawlers/getReachability.js
@@ -13,6 +13,7 @@ exports.handler = async function (context, event, callback) {
 
         const reachabilityTable = '#guidelineTable_c4 > div > div > table > tbody'
         const reachabilityTable_2 = '#guideline-tables > div > div:nth-child(2) > div > div > table > tbody'
+        const reachabilityTable_3 = '#guidelineTable_c6 > div > div > table > tbody'
 
         if($(reachabilityTable).find('tr').children().length > 0){
             $(reachabilityTable).find('tr').each((index, row) => {
@@ -23,13 +24,23 @@ exports.handler = async function (context, event, callback) {
     
             })
         }else{
-            $(reachabilityTable_2).find('tr').each((index, row) => {
-                reachability.push({
-                    [($(row).find('td:nth-child(1)').text()).trim()]:{inbound:$(row).find('td:nth-child(2)').text().trim(), outbound:$(row).find('td:nth-child(3)').text().trim()}
+            if($(reachabilityTable_2).find('tr').children().length > 0){
+                $(reachabilityTable_2).find('tr').each((index, row) => {
+                    reachability.push({
+                        [($(row).find('td:nth-child(1)').text()).trim()]:{inbound:$(row).find('td:nth-child(2)').text().trim(), outbound:$(row).find('td:nth-child(3)').text().trim()}
+                    })
+    
                 })
-
-            })
+            }else{
+                $(reachabilityTable_3).find('tr').each((index, row) => {
+                    reachability.push({
+                        [($(row).find('td:nth-child(1)').text()).trim()]:{inbound:$(row).find('td:nth-child(2)').text().trim(), outbound:$(row).find('td:nth-child(3)').text().trim()}
+                    })
+    
+                })
+            }
         }
+       
 
     }catch (error) {
         console.error(`Error: ${error}`)
