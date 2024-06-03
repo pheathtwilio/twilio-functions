e1bf28c09890c2f15fa37b985c3012aab0a20ce4
pheathtwilio <pheath@twilio.com>
2024-06-03 15:47:17 -0500

    fixed getDTMF data for USA

--- voice-guidelines/functions/crawlers/getDTMF.js
+++ voice-guidelines/functions/crawlers/getDTMF.js
@@ -12,14 +12,26 @@ exports.handler = async function (context, event, callback) {
         const $ = cheerio.load(data)
 
         const dtmfTable = '#guideline-tables > div > div:nth-child(4) > div > div > table > tbody'
+        const dtmfTable_2 = '#base-page-7c9c6637a6 > main > section.section-wrapper.default > div > div:nth-child(4) > div > div > table > tbody'
 
-        $(dtmfTable).find('tr').each((index, row) => {
+        if($(dtmfTable).find('tr').children().length > 0){
+            $(dtmfTable).find('tr').each((index, row) => {
 
-            dtmfs.push({
-                [($(row).find('td:nth-child(1)').text()).trim()]:{inbound:$(row).find('td:nth-child(2)').text().trim(), outbound:$(row).find('td:nth-child(3)').text().trim()}
+                dtmfs.push({
+                    [($(row).find('td:nth-child(1)').text()).trim()]:{inbound:$(row).find('td:nth-child(2)').text().trim(), outbound:$(row).find('td:nth-child(3)').text().trim()}
+                })
+    
             })
+        }else{
+            $(dtmfTable_2).find('tr').each((index, row) => {
 
-        })
+                dtmfs.push({
+                    [($(row).find('td:nth-child(1)').text()).trim()]:{inbound:$(row).find('td:nth-child(2)').text().trim(), outbound:$(row).find('td:nth-child(3)').text().trim()}
+                })
+    
+            })
+        }
+        
 
     }catch (error) {
         console.error(`Error: ${error}`)
